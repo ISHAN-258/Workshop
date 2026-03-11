dcc702933304644ebd90e8b0bdcc28d9deecb46e
ISHAN-258 <ishan2004tripathi@gmail.com>
2026-03-11 11:45:38 +0530

    Add files via upload

---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class MethodOverloading
+    {
+        internal void Area(int a)
+        {
+            Console.WriteLine("Area of Square: " + (a * a));
+        }
+        internal void Area(int l, int b)
+        {
+            Console.WriteLine("Area of Rectangle: " + (l * b));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOverloading m = new MethodOverloading();
+            m.Area(5);
+            m.Area(5, 10);
+        }
+    }
+}
---
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+    internal class Program
+    {
+        string CheckNumber(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+                return "Odd";
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            string r;
+            Program p = new Program();
+            Console.Write("Enter anu no. : ");
+            num = int.Parse(Console.ReadLine());
+            r = p.CheckNumber(num);
+            Console.WriteLine("The no. is : " + r);
+        }
+    }
+}
