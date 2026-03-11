3ffa9a9ddf214a0ea2aa59c5ae6065eb5be54dfc
ISHAN-258 <ishan2004tripathi@gmail.com>
2026-03-11 11:28:17 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            float salary;
+
+            Console.Write("Enter Name:");
+            name = Console.ReadLine();
+            Console.Write("Enter Age:");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Enter Salary:");
+            salary = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}, Age: {1}, Salary: {2}", name, age, salary);
+
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]nums= new int[10];
+            int x;
+            Console.Write("Enter any 10 numbers:");
+            for (x = 0; x < nums.Length; x++)
+            {
+                nums[x] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(nums);
+            Console.WriteLine("Sorted Items are: ");
+            for (x = 0; x < nums.Length; x++)
+            {
+                Console.WriteLine(nums[x]);
+            }
+        }
+    } 
+}
